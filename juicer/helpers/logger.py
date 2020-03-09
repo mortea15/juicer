86f457243dab9c6750fc3493374dab8c5897ca04
mortea15 <m.amundsen@pm.me>
2020-03-09 10:38:56 +0100

    set default lvl to 30/warn

--- juicer/helpers/logger.py
+++ juicer/helpers/logger.py
@@ -6,6 +6,7 @@ filename = 'juicer'
 
 logFormatter = logging.Formatter('%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s')
 rootLogger = logging.getLogger()
+rootLogger.setLevel(30)
 
 fileHandler = logging.FileHandler(f'{filename}.log')
 fileHandler.setFormatter(logFormatter)
@@ -24,10 +25,8 @@ def increase_log_level():
         10: 'DEBUG'
     }
     current = rootLogger.level
-    logging.debug(f'Current log level: {levels.get(current)} ({current})')
     if current > 10:
         new_level = current - 10
         if new_level % 10 == 0 and new_level in range(10,51):
-            logging.debug(f'Setting log level to {levels.get(new_level)} ({new_level})')
             rootLogger.setLevel(current - 10)
-            logging.debug(f'Set log level to {levels.get(new_level)} ({new_level})')
+            logging.info(f'Set log level to {levels.get(new_level)}')
